b145d75195a70ccda32844a0c36d0942a4e3281c
Jefferson <jefferson.echeverry@pragma.com.co>
2020-11-22 13:40:48 -0500

    Se ajusta para desplegar en heroku

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var artistRoute = require('./routes/artistRoute');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.set('port', config.port);
 
 // Ruta Base
