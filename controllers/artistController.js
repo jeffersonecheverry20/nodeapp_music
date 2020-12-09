cff89a0282c4d3bdf39d69647a71d39dba55c70f
Jefferson <jefferson.echeverry@pragma.com.co>
2020-12-09 15:02:48 -0500

    Ajuste imagen artist

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -229,11 +229,15 @@ exports.loginUser = (req, res) => {
 
 exports.uploadImage = (req, res) => {
     const artistId = req.params.id;
+    console.log(artistId);
 
     if (req.files) {
         var file_path = req.files.image.path;
+        console.log(file_path);
         var file_split = file_path.split('\\');
+        console.log(file_split);
         var file_name = file_split[2];
+        console.log(file_name);
 
         var ext_split = file_name.split('.');
         var ext = ext_split[1];
